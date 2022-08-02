601c7f31ac859e92cb1da10f82d25165a3817f9d
rbenmeddour <rayan.benmeddour@uppli.fr>
2022-08-02 13:41:49 +0200

    all except css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const request = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const response = await request.json();
+    setPokemon(response);
+  };
+  // console.log(pokemon.types);
+
+  const randomPokemon = () => {
+    const random = Math.floor(Math.random() * 151) + 1;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  };
+
+  if (!pokemon) {
+    return <div></div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={pokemon.sprites.other["official-artwork"].front_default}></img>
+      <h1>{pokemon.name}</h1>
+      <p>Height : {pokemon.height}</p>
+      <p>Weight : {pokemon.weight}</p>
+      <p>Type :</p>
+      <ul>
+        {pokemon.types.map((type) => (
+          <li>{type.type.name}</li>
+        ))}
+      </ul>
+      <button onClick={randomPokemon}>Random</button>
     </div>
   );
-}
+};
 
 export default App;
